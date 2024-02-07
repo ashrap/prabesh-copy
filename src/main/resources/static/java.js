f0b69cb141295830aa1d4b1bbea674ad67c770b3
Prabesh Khatri <prkha2082@oslomet.no>
2024-02-07 14:45:05 +0100

    update

--- src/main/resources/static/java.js
+++ src/main/resources/static/java.js
@@ -1,51 +1,58 @@
+let billetene = [];
+
 function show() {
-    let count = 0;
 
-    let kundene = [];
-
-    let film = document.getElementById("filmValg").value;
+    let filmValg = document.getElementById("filmValg").value;
     let antall = document.getElementById("antall").value;
-    let antall2 = Number(antall);
+    let antalll_value = Number(antall);
     let fornavn = document.getElementById("fornavn").value;
     let etternavn = document.getElementById("etternavn").value;
     let telefonnr = document.getElementById("telefonnr").value;
     let epost = document.getElementById("epost").value;
-    let person = {
-        movie: film,
-        total_ticket: antall2,
-        first_name: fornavn,
-        last_name: etternavn,
-        phone: telefonnr,
-        email: epost
-    };
-    kundene.push(person);
 
-    while (count <= kundene.length) {
-        let html = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>"
-        for (let i of kundene) {
-            html += "<tr>" + //we add in the html var at the same time html + object properties
-                "<td>" + i.movie + "</td>" +
-                "<td>" + i.total_ticket + "</td>" +
-                "<td>" + i.first_name + "</td>" +
-                "<td>" + i.last_name + "</td>" +
-                "<td>" + i.phone + "</td>" +
-                "<td>" + i.email + "</td>" +
-                "</tr>";
-        }
-        html += "</table>";  // we close the table tag opened earlier
-        document.getElementById("billettene").innerHTML = html; //show the result in the browser
-        count++;
+    let person = {
+        filmValg: filmValg,
+        antall: antalll_value,
+        fornavn: fornavn,
+        etternavn: etternavn,
+        telefonnr: telefonnr,
+        epost: epost
     }
 
-    document.getElementById("antall").value= " ";
+    billetene.push(person);
+
+    document.getElementById("antall").value = " ";
     document.getElementById("fornavn").value = " ";
     document.getElementById("etternavn").value = " ";
     document.getElementById("telefonnr").value = " ";
     document.getElementById("epost").value = " ";
 
-    this.inner = function clearKundene(){
-        kundene.length = 0;
+    let html = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>"
+    for (let i of billetene) {
+        html += "<tr>" + //we add in the html var at the same time html + object properties
+            "<td>" + i.filmValg + "</td>" +
+            "<td>" + i.antall + "</td>" +
+            "<td>" + i.fornavn + "</td>" +
+            "<td>" + i.etternavn + "</td>" +
+            "<td>" + i.telefonnr + "</td>" +
+            "<td>" + i.epost + "</td>" +
+            "</tr>";
+    }
+    html += "</table>";  // we close the table tag opened earlier
+    document.getElementById("billettene").innerHTML = html; //show the result in the browser
+
+    for (let i of billetene) {
+        console.log(i);
     }
 }
 
+function slett_billettene(){
+    billetene = [];
 
+    let html = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr></table>"
+
+    document.getElementById("billettene").innerHTML = html; //show the result in the browser
+
+    console.log("length of billettene is "+billetene.length);
+
+}
