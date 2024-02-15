2da8fbde90c915d682eb111e3549c87b765a82e1
Prabesh Khatri <prkha2082@oslomet.no>
2024-02-15 08:49:55 +0100

    added films and test to check email pattern and phone number pattern.

--- src/main/resources/static/java.js
+++ src/main/resources/static/java.js
@@ -1,3 +1,11 @@
+function increaseWidth(x){
+    var numberOfCharacters = x.value.length;
+    if(numberOfCharacters >= 3){
+        var length = numberOfCharacters + "ch";
+        x.style.width = length;
+    }
+}
+
 let billetene = [];
 
 function show() {
@@ -9,18 +17,32 @@ function show() {
     let etternavn = document.getElementById("etternavn").value;
     let telefonnr = document.getElementById("telefonnr").value;
     let epost = document.getElementById("epost").value;
-    let regExp = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm // test for epost
-    let regExp1 = /^[0-9]+$/ // test for telefonnr
+    let regExp = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm; // test for epost
+    let regExp1 = /^(0047|\+47|47)?[2-9]\d{7}$/; // test for telefonnr
 
-    if(antall.length === 0 || fornavn.length === 0 || etternavn.length === 0 || telefonnr.length === 0 || epost.length === 0){
+    if (filmValg == "Velg Film Her" && antall == "" && fornavn == "" && etternavn == "" && telefonnr == "" && epost == "") {
+        document.getElementById("filmValgError").innerHTML = "Må velge filmen.";
         document.getElementById("antallError").innerHTML = "Må skrive noe inn in antall."
         document.getElementById("fornavnError").innerHTML = "Må skrive noe inn i fornavnet";
         document.getElementById("etternavnError").innerHTML = "Må skrive noe inn i ettternavnet";
         document.getElementById("telefonError").innerHTML = "Må skrive noe inn i telefonnr";
         document.getElementById("epostError").innerHTML = "Må skrive noe inn i epost";
-    }
-    else {
-        if(regExp.test(epost) && regExp1.test(telefonnr)) {
+    } else {
+        if (regExp1.test(telefonnr)===false) {
+            document.getElementById("filmValgError").innerHTML = " ";
+            document.getElementById("antallError").innerHTML = " "
+            document.getElementById("fornavnError").innerHTML = " ";
+            document.getElementById("etternavnError").innerHTML = " ";
+            document.getElementById("telefonError").innerHTML = "Feil telefon nummer";
+            document.getElementById("epostError").innerHTML = " ";
+        } else if (regExp.test(epost)===false) {
+            document.getElementById("filmValgError").innerHTML = " ";
+            document.getElementById("antallError").innerHTML = " "
+            document.getElementById("fornavnError").innerHTML = " ";
+            document.getElementById("etternavnError").innerHTML = " ";
+            document.getElementById("telefonError").innerHTML = " ";
+            document.getElementById("epostError").innerHTML = "Feil Epost";
+        } else {
             let person = {
                 filmValg: filmValg,
                 antall: antalll_value,
@@ -30,8 +52,11 @@ function show() {
                 epost: epost
             }
 
+
             billetene.push(person);
 
+            // Initialisering av verdiene
+            document.getElementById("filmValg").value = "Velg Film Her";
             document.getElementById("antall").value = " ";
             document.getElementById("fornavn").value = " ";
             document.getElementById("etternavn").value = " ";
@@ -41,28 +66,25 @@ function show() {
             let html = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>"
             for (let i of billetene) {
                 html += "<tr>" + //we add in the html var at the same time html + object properties
-                    "<td>" + i.filmValg + "</td>" +
-                    "<td>" + i.antall + "</td>" +
-                    "<td>" + i.fornavn + "</td>" +
-                    "<td>" + i.etternavn + "</td>" +
-                    "<td>" + i.telefonnr + "</td>" +
-                    "<td>" + i.epost + "</td>" +
+                    "<td align='center'>" + i.filmValg + "</td>" +
+                    "<td align='center'>" + i.antall + "</td>" +
+                    "<td align='center'>" + i.fornavn + "</td>" +
+                    "<td align='center'>" + i.etternavn + "</td>" +
+                    "<td align='center'>" + i.telefonnr + "</td>" +
+                    "<td align='center'>" + i.epost + "</td>" +
                     "</tr>";
             }
             html += "</table>";  // we close the table tag opened earlier
             document.getElementById("billettene").innerHTML = html; //show the result in the browser
 
+            document.getElementById("filmValgError").innerHTML = " ";
             document.getElementById("antallError").innerHTML = " "
             document.getElementById("fornavnError").innerHTML = " ";
             document.getElementById("etternavnError").innerHTML = " ";
             document.getElementById("telefonError").innerHTML = " ";
             document.getElementById("epostError").innerHTML = " ";
         }
-        else {
-            alert("Please enter valid email address.")
-        }
     }
-
 }
 
 function slett_billettene() {
